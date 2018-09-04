fa67a9e0dca74dafc415beae8489b52d7fbb2599
dearbear <dearbear_zhang@icloud.com>
2018-09-04 17:52:01 +0800

    //新增xml转javabean

--- MyTest/src/main/java/com/my/xml/XmlConvertUtil.java
+++ MyTest/src/main/java/com/my/xml/XmlConvertUtil.java
@@ -4,11 +4,18 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class XmlConvertUtil {
+    /***
+     * xml转javaBean
+     * @param xmlString
+     * @param cls
+     * @param <T>
+     * @return
+     */
     public static <T extends Object> T toJavaBean(String xmlString, Class cls) {
         T javaBean = null;
         XStream xstream = new XStream(new DomDriver());
-        XStream.setupDefaultSecurity(xstream);
-        xstream.allowTypes(new Class[]{cls});
+//        XStream.setupDefaultSecurity(xstream);
+//        xstream.allowTypes(new Class[]{cls});
         xstream.ignoreUnknownElements();
         xstream.processAnnotations(cls);
         try {
@@ -17,5 +24,20 @@ public class XmlConvertUtil {
             e.printStackTrace();
         }
         return javaBean;
+    }
+
+    /***
+     * JavaBean 转 xml字符串
+     * @param object
+     * @param cls
+     * @return
+     */
+    public static String toXml(Object object, Class cls) {
+        String xml = null;
+        XStream xstream = new XStream(new DomDriver());
+        xstream.ignoreUnknownElements();
+        xstream.processAnnotations(cls);
+        xml = xstream.toXML(object);
+        return xml;
     }
 }
